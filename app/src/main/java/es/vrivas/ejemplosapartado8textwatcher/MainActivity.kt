95a87d2121b5ed7979f1225b5ab3cc15f869cd50
Victor Rivas <vrivas@ujaen.es>
2020-11-01 17:05:31 +0100

    Tercera versión de captura de eventos. Una clase interna (inner) es el manejador de eventos.

--- app/src/main/java/es/vrivas/ejemplosapartado8textwatcher/MainActivity.kt
+++ app/src/main/java/es/vrivas/ejemplosapartado8textwatcher/MainActivity.kt
@@ -3,12 +3,28 @@ package es.vrivas.ejemplosapartado8textwatcher
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import es.vrivas.ejemplosapartado8textwatcher.R.id.et_palabra
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var lista = listOf<String>("ahorro", "barco", "barquito", "boquerón", "fichero", "ficticio", "tender", "tendencia", "tomar")
+
+    // 3ª Versión de la captura de eventos: una clase adicional maneja los eventos.
+    // Al declarar la clase como inner podemos acceder a todos los métodos de la clase contenedora
+    inner class ManejadorEventos: TextWatcher {
+        // Imprescindible ponerlo, porque TextWatcher es abstracto
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        // Imprescindible ponerlo, porque TextWatcher es abstracto
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        // Actualiza la lista de palabras
+        override fun afterTextChanged(s: Editable?) {
+            val coinciden = seleccionar_palabras(et_palabra.text.toString())
+            tv_lista_palabras.text=coinciden.toString()
+        }
+    }
 
     // Este método filtra las palabras que empiezan por las letras introducidas por el usuario
     private fun seleccionar_palabras(raiz: String): MutableList<String> {
@@ -26,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         //et_palabra.addTextChangedListener(this)
         */
 
+        /*
         // 2ª Versión de la captura de eventos: el propio widget captura el evento
         // Al mismo tiempo que le añado el listener, declaro el objeto que manejará los eventos
         et_palabra.addTextChangedListener( object: TextWatcher {
@@ -44,6 +61,11 @@ class MainActivity : AppCompatActivity() {
                 tv_lista_palabras.text=coinciden.toString()
             }
         })
+        */
+
+        // 3ª Versión de la captura de eventos: una clase adicional maneja los eventos.
+        // Indicamos qué clase es la que manejará los eventos.
+        et_palabra.addTextChangedListener(ManejadorEventos())
 
     }
 
