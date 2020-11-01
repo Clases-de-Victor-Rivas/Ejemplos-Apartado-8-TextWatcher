0bebbaedb2b2b04032890faa960949c80a7abfbe
Victor Rivas <vrivas@ujaen.es>
2020-11-01 16:23:00 +0100

    Segunda versión de captura de eventos. El propio widget es el manejador de eventos.

--- app/src/main/java/es/vrivas/ejemplosapartado8textwatcher/MainActivity.kt
+++ app/src/main/java/es/vrivas/ejemplosapartado8textwatcher/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), TextWatcher {
+class MainActivity : AppCompatActivity() {
     var lista = listOf<String>("ahorro", "barco", "barquito", "boquerón", "fichero", "ficticio", "tender", "tendencia", "tomar")
 
     // Este método filtra las palabras que empiezan por las letras introducidas por el usuario
@@ -20,12 +20,35 @@ class MainActivity : AppCompatActivity(), TextWatcher {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 1ª Versión de la captura de eventos
+        /*
+        // 1ª Versión de la captura de eventos: la actividad captura el evento
         // Indico al EditText et_palabra que su manejador de eventos es la propia actividad
-        et_palabra.addTextChangedListener(this)
+        //et_palabra.addTextChangedListener(this)
+        */
+
+        // 2ª Versión de la captura de eventos: el propio widget captura el evento
+        // Al mismo tiempo que le añado el listener, declaro el objeto que manejará los eventos
+        et_palabra.addTextChangedListener( object: TextWatcher {
+
+            // Imprescindible ponerlo, porque TextWatcher es abstracto
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            // Imprescindible ponerlo, porque TextWatcher es abstracto
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            // Actualiza la lista de palabras
+            override fun afterTextChanged(s: Editable?) {
+                val coinciden = seleccionar_palabras(et_palabra.text.toString())
+                tv_lista_palabras.text=coinciden.toString()
+            }
+        })
+
     }
 
-    // 1ª Versión de la captura de eventos.
+     /*
+    // 1ª Versión de la captura de eventos: la actividad captura el evento
     // Tengo que implementar TODOS los métodos abstractos, aunque no se usen
 
     // Imprescindible ponerlo, porque TextWatcher es abstracto
@@ -41,4 +64,5 @@ class MainActivity : AppCompatActivity(), TextWatcher {
         val coinciden = seleccionar_palabras(et_palabra.text.toString())
         tv_lista_palabras.text=coinciden.toString()
     }
+    */
 }
